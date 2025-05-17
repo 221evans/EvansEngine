c36377396df4a544a4d511c9b228f6ecccd07e07
221evans <jayevo95@gmail.com>
2025-05-17 19:05:51 +0100

    Initial commit

--- Core/game.cpp
+++ Core/game.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "game.hpp"
+
+#include <iostream>
+#include <__ostream/basic_ostream.h>
+
+Game::Game()
+{
+
+}
+
+
+void Game::GameInit(SDL_Renderer *renderer)
+{
+    texture = nullptr;
+    texture = IMG_LoadTexture(renderer,"Resources/Background/BackgroundImage.png");
+}
+
+
+void Game::GameRender(SDL_Renderer *renderer)
+{
+    if (texture == nullptr)
+    {
+        std::cout << "Background Texture is null" << std::endl;
+    }
+
+    SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+}
+
+void Game::GameUpdate(float deltaTime)
+{
+}
+
+Game::~Game()
+{
+    if (texture != nullptr)
+    {
+        SDL_DestroyTexture(texture);
+        texture = nullptr;
+    }
+}
--- Core/game.hpp
+++ Core/game.hpp
@@ -5,9 +5,27 @@
 #ifndef GAME_HPP
 #define GAME_HPP
 
+#include "SDL3/SDL.h"
+#include "SDL3_image/SDL_image.h"
 
 
 class Game {
+
+    public:
+        Game();
+        ~Game();
+
+    // Variables
+    SDL_Texture *texture;
+
+    // Methods
+    void GameInit(SDL_Renderer *renderer);
+    void GameRender(SDL_Renderer *renderer);
+    void GameUpdate(float deltaTime);
+
+    private:
+
+
 
 };
 
